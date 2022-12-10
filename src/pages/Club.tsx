0eb8ae70f79bdadcb7551662447ee6fa433e1f4e
geunsoo <geunsoo1106@gmail.com>
2022-12-10 19:35:42 +0900

    구단 컴포넌트 마크업 진행 중

--- src/pages/Club.tsx
+++ src/pages/Club.tsx
@@ -1,4 +1,52 @@
+import styled from "styled-components";
+import Header from "@/components/header";
+import ClubItem from "./ClubItem";
+const ClubContainer = styled.div`
+  width: 1700px;
+  height: 700px;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
+// const ClubBox = styled.div`
+//   width: 300px;
+//   height: 250px;
+//   margin: 50px;
+//   border: 2px solid #aaa;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+// `;
 const Club = () => {
-  return;
+  const clubImg = [
+    "/img/대구FC.png",
+    "/img/대전 시티즌.png",
+    "/img/수원 블루윙즈.png",
+    "/img/울산 현대.png",
+    "/img/인천 유나이티드.png",
+    "/img/FC서울.png",
+    "/img/전북 현대 모터스.png",
+    "/img/제주 유나이티드.png",
+    "/img/포항 스틸러스.png",
+    "/img/강원FC.png",
+    "/img/부산 아이파크.png",
+    "/img/성남 일화.png",
+    "/img/전남 드래곤즈.png",
+    "/img/광주FC.png",
+    "/img/경남FC.png",
+    "/img/상주 상무.png",
+  ];
+  return (
+    <ClubContainer>
+      {/* <ClubBox> */}
+      {clubImg.map((value, index) => {
+        return <ClubItem key={index} data={value}></ClubItem>;
+      })}
+      {/* </ClubBox> */}
+    </ClubContainer>
+  );
 };
 export default Club;
---
+++ src/pages/ClubItem.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+const ClubLogo = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const ClubName = styled.div`
+  color: white;
+  font-family: esaLight;
+  font-size: 1.5rem;
+`;
+const ClubItem = ({ data }: any) => {
+  const ClubName = [
+    "대구FC",
+    "대전시티즌",
+    "수원삼성블루윙즈",
+    "울산현대",
+    "인천유나이티드",
+    "FC서울",
+    "전북현대",
+    "제주유나이티드",
+    "포항스틸러스",
+    "강원FC",
+    "부산아이파크",
+    "성남일화",
+    "전남드래곤즈",
+    "광주FC",
+    "경남FC",
+    "상주상무",
+  ];
+  return (
+    <>
+      <ClubLogo src={data}></ClubLogo>
+
+      {/* <ClubName>대전시티즌</ClubName> */}
+    </>
+  );
+};
+export default ClubItem;
