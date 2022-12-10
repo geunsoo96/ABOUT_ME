f703f65b19fc818c1d460060950ddddb75e78be5
geunsoo <geunsoo1106@gmail.com>
2022-12-10 19:35:03 +0900

    Outlet 기능 추가

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,7 @@
 import Header from "./header";
 import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+import Top from "./top";
 const Img = styled.div`
   margin: 0;
   padding: 0;
@@ -7,11 +9,17 @@ const Img = styled.div`
   background-size: cover;
   width: 1920px;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 `;
+
 const Main = () => {
   return (
     <Img>
       <Header />
+      <Outlet />
     </Img>
   );
 };
