44fac8276b1394e5e465f8ab7008622ca877549a
geunsoo <geunsoo1106@gmail.com>
2022-12-11 18:53:07 +0900

    순위, 뉴스 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Header from "./components/header";
-import Club from "./pages/Club";
-import News from "./pages/News";
-import Ranking from "./pages/Ranking";
-import Schedule from "./pages/Schedule";
+import Club from "./pages/Club/Club";
+import News from "./pages/News/News";
+import Ranking from "./pages/Ranking/Ranking";
+import Schedule from "./pages/Schedule/Schedule";
 import Main from "./components/main";
 import Top from "./components/top";
 import styled from "styled-components";
@@ -15,6 +15,8 @@ const App = () => {
         <Route path="/" element={<Main />}>
           <Route path="/" element={<Top />}></Route>
           <Route path="/club" element={<Club />}></Route>
+          <Route path="/rank" element={<Ranking />}></Route>
+          <Route path="/news" element={<News />}></Route>
         </Route>
       </Routes>
     </>
